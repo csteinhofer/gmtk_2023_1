3a5efb9cc7fbd243fb75b6a16e82e7cfff79793f
Christian Steinhofer <cmsteinhofer@gmail.com>
2023-07-08 21:40:23 -0500

    animation and movmenet for player

--- Undelivery/Assets/Scripts/PlayerMovement.cs
+++ Undelivery/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Animator my_anime;
 
     Vector2 movement;
 
@@ -19,12 +20,23 @@ public class PlayerMovement : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
 
         
+        
+        
     }
     private void FixedUpdate()
     {
-        //movement
+        //movement & animation
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        my_anime.SetFloat("Horizontal", movement.x);
+        my_anime.SetFloat("Vertical", movement.y);
 
+        my_anime.SetFloat("Speed", movement.sqrMagnitude);
+
+        //set last direction for proper transition to idle anim
+        if (movement.sqrMagnitude > 0) {
+            my_anime.SetFloat("LastDirHor", movement.x);
+            my_anime.SetFloat("LastDirVer", movement.y);
+        }
         
     }
 }
